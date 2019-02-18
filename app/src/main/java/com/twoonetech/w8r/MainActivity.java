ca08e234d576a26122ef23efff7b2426553921ae
jaydn <jaydn.1999@hotmail.com>
2019-02-18 18:51:45 +0000

    Created fragment and made changes to main activity and view model

--- app/src/main/java/com/twoonetech/w8r/MainActivity.java
+++ app/src/main/java/com/twoonetech/w8r/MainActivity.java
@@ -3,6 +3,8 @@ package com.twoonetech.w8r;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
@@ -15,9 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("App start", "App start");
 
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
         RobotsViewModel model = ViewModelProviders.of(this).get(RobotsViewModel.class);
 
         Button searchForRobots = findViewById(R.id.search_for_robots);
+        searchForRobots.setOnClickListener(view -> {
+
+        });
+
+        Button help = findViewById(R.id.help);
         searchForRobots.setOnClickListener(view -> {
 
         });
--- app/src/main/java/com/twoonetech/w8r/RobotsViewModel.java
+++ app/src/main/java/com/twoonetech/w8r/RobotsViewModel.java
@@ -4,14 +4,15 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
+import java.util.List;
 import java.util.Map;
 
 public class RobotsViewModel extends ViewModel {
 
-    //robots is a mapping between the ip addresses of each robot to an object which contains the status of the robot
-    private MutableLiveData<Map<String,Object>> liveRobotData;
+    //liveRobotIps is a live list of the robot ips that are on the same network
+    private MutableLiveData<List<String>> liveRobotIps;
 
-    public LiveData<Map<String,Object>> getLiveRobotData() {
+    public LiveData<Map<String,Object>> getLiveRobotData(){
         if (liveRobotData == null) {
             //If the live data is null, instantiate it with a new MutableLiveData and set it's value to null
             liveRobotData = new MutableLiveData<>();
@@ -20,13 +21,14 @@ public class RobotsViewModel extends ViewModel {
         return liveRobotData;
     }
 
-    public void updateUserData(Map<String,Object> robotData) {
+    public void updateUserData(Map<String,Object> robotData){
         //Update the local robot data
         liveRobotData.setValue(robotData);
     }
 
     public Object getSpecificRobotData(String ip){
-        //if given ip is not in the set of robots
+        //If given ip is not in the set of robots
+        Methods.sendCommand("","","getStatus",new String[]{});
         Object robotData;
         if (!liveRobotData.getValue().keySet().contains(ip)){
             robotData = null;
@@ -36,6 +38,11 @@ public class RobotsViewModel extends ViewModel {
         return robotData;
     }
 
-
+    //Use DHCP to get ip address of all devices on network that ARE robots and put it into the live list
+    public List<String> searchForRobots(){
+        List<String> ips; //DO DHCP STUFF HERE
+        liveRobotIps.postValue(ips); //Put updated list of ips into the live list
+        return ips;
+    }
 
 }
